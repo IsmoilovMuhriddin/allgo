fa39d3d95e3312c6650b5d85548a15e36cf4573c
Muhriddin <ismoilovmuh1996@gmail.com>
2017-10-27 01:20:40 +0900

    speed motor

--- motor.py
+++ motor.py
@@ -8,9 +8,15 @@ import time
 pca = PCA9685()
 
 def main():
-    sp=int(input('input Speed'))
-    pca.go_left(speed_cur=sp,turning_rate=0.75)
-
+    """ test for motor"""
+    print 'choose mode'
+    mod=int(input())
+    if(mod==1):
+        sp=int(input('input Speed'))
+        pca.go_left(speed_cur=sp,turning_rate=0.75)
+    elif mod==2:
+        sp = int(input('input Speed'))
+        pca.go_right(speed_cur=sp, turning_rate=0.75)
 
 
 if __name__ == "__main__":
