3c2fac898c012426d6225d28de297c3d27f43771
Muhriddin <ismoilovmuh1996@gmail.com>
2017-10-27 00:14:02 +0900

    turning rate

--- motor.py
+++ motor.py
@@ -1,6 +1,6 @@
 import cv2 
 import numpy as np
-from allgo_utils import PCA9685,ultrasonic,ir_sens
+from .. import allgo_utils # allgo_utils import PCA9685,ultrasonic,ir_sens
 import wiringpi as wp
 import time
 
