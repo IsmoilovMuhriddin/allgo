0d90959b8fd75952bc7b180c8e89be4e46901225
Muhriddin <ismoilovmuh1996@gmail.com>
2017-11-19 02:27:00 +0900

    client is here

--- ultra_client.py
+++ ultra_client.py
@@ -1,6 +1,6 @@
 from socket import *
 import time
-from allgo_utils import *
+from allgo_utils import ultrasonic
 ULTRASONIC_TRIG	= 3 # TRIG port is to use as output signal
 ULTRASONIC_ECHO = 23 # ECHO port is to use as input signal
 
@@ -14,7 +14,7 @@ ult=ultrasonic(ULTRASONIC_TRIG,ULTRASONIC_ECHO)
 try:
     while True:
         print "starting measure"
-        dist= ult.distance()
+        dist = ult.distance()
         print "Distance: %.1f cm" % dist
         client_socket.send(str(dist))
         time.sleep(0.5)
